1f1add83a8226397b3be013a9e2d8482469fc83b
rozul057 <82423136+rozul057@users.noreply.github.com>
2021-05-08 16:15:42 -1200

    Create operasibilangan.java

---
+++ operasibilangan.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul4;
+
+public class operasibilangan {
+    protected double a,b,c ;
+    
+    protected void setA(double a){
+        this.a = a;
+    }
+    
+    protected void setB(double b){
+        this.b = b;
+    }
+    
+    protected void setC(){
+
+    }
+    
+    protected double getA(){
+        return a;
+    }
+    
+    protected double getB(){
+        return b;
+    }
+    
+    protected double getC(){
+        return c;
+    }
+    
+    protected void tampil(){
+    }
+     
+}
+    
+
+    
+
+
+
+
