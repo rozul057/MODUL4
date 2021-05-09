b2db8428e350f06f21d46a33edb4c24d1e8517a1
rozul057 <82423136+rozul057@users.noreply.github.com>
2021-05-08 16:16:58 -1200

    Create operasipengurangan.java

---
+++ operasipengurangan.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul4;
+
+public class operasipengurangan extends operasibilangan {
+    
+     @Override
+    protected void setA(double a){
+        this.a = a;
+    }
+    
+     @Override
+    protected void setB(double b){
+        this.b = b;
+    }
+    
+     @Override
+    protected void setC(){
+        this.c = a-b;
+
+    }
+    
+    @Override
+    protected double getA(){
+        return a;
+    }
+    
+     @Override
+    protected double getB(){
+        return b;
+    }
+    
+     @Override
+    protected double getC(){
+        return c;
+    }
+    
+     @Override
+    protected void tampil(){
+    } 
+}
+
+
